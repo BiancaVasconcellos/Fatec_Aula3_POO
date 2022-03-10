b2861feea913ab1062fa08e416c6e414b6fff7bd
Bianca <biancakrafecik@hotmail.com>
2022-03-10 17:39:17 -0300

    Atualizando repositório

---
+++ OperadorTernario.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class OperadorTernario {
+
+    public static void main(String [] args) {
+        byte nota =  Byte.parseByte(
+            JOptionPane.showInputDialog("Qual a nota")
+        );
+
+        JOptionPane.showMessageDialog(
+            null,
+            nota >= 6 ? "Aprovado" : "Reprovado"
+        );    
+
+
+    }
+    
+}
